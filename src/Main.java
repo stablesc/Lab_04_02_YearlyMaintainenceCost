eefdab1351a3d86316e4ceabcbf07dae3df63667
Steele <stablesc@mail.uc.edu>
2022-09-23 00:08:24 -0400

    final commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+        double springCost = 100;
+        double summerCost = 150;
+        double autumnCost = 80;
+        double winterCost = 200;
+        double yearlyCost = springCost + summerCost + autumnCost + winterCost;
+        {
+            System.out.println(" When the maintenance cost for Spring is $" + springCost + ", the Summer cost is $"
+                    + summerCost + ", the Autumn cost is $" + autumnCost + ", and the Winter cost is $" + winterCost +
+                    ", the total yearly maintenance cost is $" + yearlyCost + ".");
+        }
     }
 }
